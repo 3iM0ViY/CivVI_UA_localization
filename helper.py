cb38a57219c4e960729c45d043680f84272a9a75
Yulian Tolkachov <56gymnasia@gmail.com>
2023-11-02 21:14:00 +0100

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,32 @@
+# н-на кількість рядків для перекладу 
+l = """<Row Tag="LOC_READY_UNSUPPORTED">
+			<Text>Unsupported.</Text>
+		</Row>
+		"""
+ 
+import re
+ 
+names = re.findall("<Text>.+</Text>", l)
+print(names)
+names_2 = []
+for i in names:
+    i = i.replace("<Text>", "")
+    i = i.replace("</Text>", "")
+    names_2.append(i)
+    
+for i in names_2:
+    print(i) # чстий текст для перекладу
+    
+# перекладений текст
+tr = """
+
+"""
+
+tr_l = []
+for line in tr.splitlines():
+    tr_l.append(line)
+
+print(tr_l)
+
+for i in tr_l:
+    print(f"<Text>{i}</Text>") # форматування перекладеного тексту
